6f8cdcb24631c8ba653c996c68a6a9ee454045dd
Oleg <72274260+SoulofAO@users.noreply.github.com>
2020-11-27 21:35:04 +0400

    Djungo 2

--- SmartSite/SmartSite/apps/osn/admin.py
+++ SmartSite/SmartSite/apps/osn/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Priloshenie,ButtonFind,Comment
+admin.site.register(Priloshenie)
+admin.site.register(ButtonFind)
+admin.site.register(Comment)
--- SmartSite/SmartSite/apps/osn/models.py
+++ SmartSite/SmartSite/apps/osn/models.py
@@ -2,10 +2,29 @@ from django.db import models
 import time
 # Create your models here.
 class Priloshenie((models.Model)):
-    Name=models.CharField()
-    Text = models.CharField()
-    Pub_Date=models.TimeField()
+    Name=models.CharField("название", max_length=200)
+    Text = models.TextField("Текст")
+    Pub_Date=models.DateTimeField("Дата создания")
     Reiting=[]
+
+    def __str__(self):
+        return self.Name
+    class Meta:
+        verbose_name="Приложение"
+
 class ButtonFind((models.Model)):
-    NameB=models.CharField()
-    Bull=models.BooleanField()
+    NameB=models.CharField("Название Кнопки",max_length=50)
+    Bull=models.BooleanField("Зажато/НеЗажато")
+
+    class Meta:
+     verbose_name = "Кнопка для поиска"
+
+
+class Comment((models.Model)):
+    priloshenie=models.ForeignKey(Priloshenie,on_delete=models.CASCADE)
+    Name=models.CharField("название", max_length=50)
+    Text = models.TextField("Текст")
+    Pub_Date=models.DateTimeField("Дата создания")
+
+    class Meta:
+     verbose_name = "Комментарий"
